0a8c3e045bb7d272a09413525411460f97fb1643
HuyLG99 <lehuy154@gmail.com>
2021-06-25 12:35:41 +0700

    Contact and About Us

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -220,11 +220,10 @@
                                 <h4>Contact Info</h4>
                                 <ul>
                                     <li>
-                                        <p>Address :Your address goes
-                                            here, your demo address.</p>
+                                        <p>Address :218 Phan Dinh Phung, Ho Chi Minh.</p>
                                     </li>
-                                    <li><a href="#">Phone : +8880 44338899</a></li>
-                                    <li><a href="#">Email : info@colorlib.com</a></li>
+                                    <li><a href="#">Phone : +8438 44338899</a></li>
+                                    <li><a href="#">Email : JObFinder@gmail.com</a></li>
                                 </ul>
                             </div>
 
@@ -309,7 +308,7 @@
                         <div class="col-xl-10 col-lg-10 ">
                             <div class="footer-copy-right">
                                 <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | <i class="fa fa-heart" aria-hidden="true"></i>
                                     <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
                             </div>
                         </div>
